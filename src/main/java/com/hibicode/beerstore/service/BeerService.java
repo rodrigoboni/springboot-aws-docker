57ceb8d5f7ddb930fdb49b93d45d80fb8ff7f5de
Rodrigo Boni <rodrigoboni@gmail.com>
2019-01-24 00:04:22 -0200

    implementando testes

--- src/main/java/com/hibicode/beerstore/service/BeerService.java
+++ src/main/java/com/hibicode/beerstore/service/BeerService.java
@@ -1,11 +1,32 @@
 package com.hibicode.beerstore.service;
 
 import com.hibicode.beerstore.model.Beer;
+import com.hibicode.beerstore.repository.Beers;
 import com.hibicode.beerstore.service.exception.BeerAlreadyExistException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public class BeerService {
+import java.util.Optional;
+
+@Service
+class BeerService {
 	
-	public Beer save(final Beer beer) {
-		throw new BeerAlreadyExistException();
+	private final Beers beers;
+	
+	// recebe a instância do repositório pelo construtor
+	// em runtime da aplicação a anotação @autowired funciona normal
+	// em runtime de testes a instância gerá passada pelo mockito (ver classes de testes)
+	BeerService(@Autowired final Beers beers) {
+		this.beers = beers;
+	}
+	
+	Beer save(final Beer beer) {
+		final Optional<Beer> beerByNameAndType = beers.findByNameAndType(beer.getName(), beer.getType());
+		
+		if(beerByNameAndType.isPresent()) {
+			throw new BeerAlreadyExistException();
+		}
+		
+		return beers.save(beer);
 	}
 }
--- src/test/java/com/hibicode/beerstore/service/BeerServiceTest.java
+++ src/test/java/com/hibicode/beerstore/service/BeerServiceTest.java
@@ -2,23 +2,40 @@ package com.hibicode.beerstore.service;
 
 import com.hibicode.beerstore.model.Beer;
 import com.hibicode.beerstore.model.BeerType;
+import com.hibicode.beerstore.repository.Beers;
 import com.hibicode.beerstore.service.exception.BeerAlreadyExistException;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.when;
 
 public class BeerServiceTest {
 	
 	private BeerService beerService;
 	
+	@Mock // anotação p/ indicar que este attr é mockado
+	private Beers beersMocked;
+	
 	@Before
 	public void setup() {
-		beerService  = new BeerService();
+		// inicializa classes marcadas com @Mock
+		MockitoAnnotations.initMocks(this);
+		
+		// injeta no serviço a instância mockada do repositório
+		beerService  = new BeerService(beersMocked);
 	}
 	
 	@Test(expected = BeerAlreadyExistException.class)
 	public void should_deny_creation_of_beer_that_exists() {
+		when(beersMocked.findByNameAndType(getTestBeer().getName(), getTestBeer().getType())).thenReturn(Optional.of(getTestBeer()));
+		
 		final Beer beer = new Beer();
 		beer.setName("Heineken");
 		beer.setType(BeerType.LAGER);
@@ -34,6 +51,22 @@ public class BeerServiceTest {
 		beer.setType(BeerType.LAGER);
 		beer.setVolume(new BigDecimal(335));
 		
-		beerService.save(beer);
+		when(beersMocked.save(beer)).thenReturn(getTestBeer());
+		
+		final Beer beerSaved = beerService.save(beer);
+		
+		assertThat(beerSaved.getId(), equalTo(getTestBeer().getId()));
+		assertThat(beerSaved.getName(), equalTo(getTestBeer().getName()));
+		assertThat(beerSaved.getType(), equalTo(getTestBeer().getType()));
+	}
+	
+	private Beer getTestBeer() {
+		final Beer beer = new Beer();
+		beer.setId(10L);
+		beer.setName("Heineken");
+		beer.setType(BeerType.LAGER);
+		beer.setVolume(new BigDecimal(335));
+		
+		return beer;
 	}
 }
